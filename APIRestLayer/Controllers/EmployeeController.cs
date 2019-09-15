d8b9132327d683c1b66a6a60f5a972803c5db755
Lucas Montelongo <lucasmontelongo@outlook.com>
2019-09-15 15:48:18 -0300

    Ejercicio 7
    Falta habilitar el CORS, lo voy a hacer una vez genere el proyecto MVC

---
+++ APIRestLayer/Controllers/EmployeeController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace APIRestLayer.Controllers
+{
+    public class EmployeeController : ApiController
+    {
+        BusinessLogicLayer.BLEmployees blHandler = new BusinessLogicLayer.BLEmployees(new DataAccessLayer.DALEmployeesEF());
+
+        public HttpResponseMessage Get()
+        {
+            try
+            {
+                List<Shared.Entities.Employee> empployees = blHandler.GetAllEmployees();
+                return Request.CreateResponse(HttpStatusCode.OK, empployees);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        public HttpResponseMessage Get(int id)
+        {
+            try
+            {
+                Shared.Entities.Employee emp = blHandler.GetEmployee(id);
+                return Request.CreateResponse(HttpStatusCode.OK, emp);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+        
+        public HttpResponseMessage Post(Shared.Entities.EmployeeAPIRest emp)
+        {
+            try
+            {
+                if(emp.TypeEmp == "FullTime")
+                {
+                    Shared.Entities.FullTimeEmployee empleado = new Shared.Entities.FullTimeEmployee()
+                    {
+                        Name = emp.Name,
+                        StartDate = Convert.ToDateTime(emp.StartDate),
+                        Salary = emp.Salary
+                    };
+                    blHandler.AddEmployee(empleado);
+                }
+                else if (emp.TypeEmp == "PartTime")
+                {
+                    Shared.Entities.PartTimeEmployee empleado = new Shared.Entities.PartTimeEmployee()
+                    {
+                        Name = emp.Name,
+                        StartDate = Convert.ToDateTime(emp.StartDate),
+                        HourlyRate = emp.HourlyRate
+                    };
+                    blHandler.AddEmployee(empleado);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Tipo de usuario incorrecto");
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, "Usuario creado correctamente");
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        public HttpResponseMessage Delete(int id)
+        {
+            try
+            {
+                blHandler.DeleteEmployee(id);
+                return Request.CreateResponse(HttpStatusCode.OK, "Usuario eliminado correctamente");
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        public HttpResponseMessage Put(Shared.Entities.EmployeeAPIRest emp)
+        {
+            try
+            {
+                if (emp.TypeEmp == "FullTime")
+                {
+                    Shared.Entities.FullTimeEmployee empleado = new Shared.Entities.FullTimeEmployee()
+                    {
+                        Id = emp.Id,
+                        Name = emp.Name,
+                        StartDate = Convert.ToDateTime(emp.StartDate),
+                        Salary = emp.Salary
+                    };
+                    blHandler.UpdateEmployee(empleado);
+                }
+                else if (emp.TypeEmp == "PartTime")
+                {
+                    Shared.Entities.PartTimeEmployee empleado = new Shared.Entities.PartTimeEmployee()
+                    {
+                        Id = emp.Id,
+                        Name = emp.Name,
+                        StartDate = Convert.ToDateTime(emp.StartDate),
+                        HourlyRate = emp.HourlyRate
+                    };
+                    blHandler.UpdateEmployee(empleado);
+                }
+                else
+                {
+                    return Request.CreateResponse(HttpStatusCode.InternalServerError, "Tipo de usuario incorrecto");
+                }
+                return Request.CreateResponse(HttpStatusCode.OK, "Usuario actualizado correctamente");
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+
+        public HttpResponseMessage GetCalcPartTimeEmployeeSalary(int id, int hours)
+        {
+            try
+            {
+                double res = blHandler.CalcPartTimeEmployeeSalary(id, hours);
+                return Request.CreateResponse(HttpStatusCode.OK, res);
+            }
+            catch (Exception e)
+            {
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, e);
+            }
+        }
+    }
+}
---
+++ CrossCutting/Entities/EmployeeAPIRest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Entities
+{
+    public class EmployeeAPIRest
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string StartDate { get; set; }
+        public int Salary { get; set; }
+        public double HourlyRate { get; set; }
+        public string TypeEmp { get; set; }
+    }
+}
