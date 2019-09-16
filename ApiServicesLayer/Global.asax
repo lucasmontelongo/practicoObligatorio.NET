9541733226e39d340bb0b1ac27cf11197d0cc9bb
Lucas Montelongo <lucasmontelongo@outlook.com>
2019-09-15 22:29:14 -0300

    Eliminando proyecto de prueba

--- ApiServicesLayer/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="ApiServices.WebApiApplication" Language="C#" %>
--- PresentationLayer/ClientApp/app/employee/manage/manage-employee.component.ts
+++
@@ -1,8 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-    selector: 'tsi1-manage-employee',
-    templateUrl: 'manage-employee.component.html',
-    styleUrls: [ 'manage-employee.component.css']
-})
-export class ManageEmployeeComponent { }
